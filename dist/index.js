7cef57b47e05124a4138226d587155423d0cc690
Kieve Chua <kievechua@gmail.com>
2017-01-25 10:57:42 +0800

    Upgrade core to v2

--- dist/index.js
+++ dist/index.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _base = require('magnet-core/dist/base');
+var _base = require('magnet-core/base');
 
 var _base2 = _interopRequireDefault(_base);
 
@@ -20,7 +20,7 @@ var _bunyan4 = _interopRequireDefault(_bunyan3);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { return step("next", value); }, function (err) { return step("throw", err); }); } } return step("next"); }); }; }
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -34,13 +34,13 @@ var Bunyan = function (_Base) {
   function Bunyan() {
     _classCallCheck(this, Bunyan);
 
-    return _possibleConstructorReturn(this, Object.getPrototypeOf(Bunyan).apply(this, arguments));
+    return _possibleConstructorReturn(this, (Bunyan.__proto__ || Object.getPrototypeOf(Bunyan)).apply(this, arguments));
   }
 
   _createClass(Bunyan, [{
     key: 'setup',
     value: function () {
-      var ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
+      var _ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
         var bunyanConfig, config;
         return regeneratorRuntime.wrap(function _callee$(_context) {
           while (1) {
@@ -64,7 +64,7 @@ var Bunyan = function (_Base) {
       }));
 
       function setup() {
-        return ref.apply(this, arguments);
+        return _ref.apply(this, arguments);
       }
 
       return setup;
